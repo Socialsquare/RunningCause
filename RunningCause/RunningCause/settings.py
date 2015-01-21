5e5d1d827297951730d2fcdc8ec2ab8e7561869d
Niles Christensen <nilesnchristensen@gmail.com>
2015-01-21 14:13:29 +0100

    [Removes] references to secrets in settings.py

--- RunningCause/RunningCause/settings.py
+++ RunningCause/RunningCause/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import secrets
+# import secrets
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
