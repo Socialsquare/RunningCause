d5b38e56d965f23e096b742b09912411d42afe15
Niles Christensen <nilesnchristensen@gmail.com>
2015-01-21 14:10:22 +0100

    [Changes] some things to hopefully make it work with heroku

--- RunningCause/RunningCause/settings.py
+++ RunningCause/RunningCause/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import secrets
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
@@ -18,9 +19,9 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Nice Try'
-RUNKEEPER_CLIENT_ID = 'sdfhksdjfh'
-RUNKEEPER_CLIENT_SECRET = 'ksejarhaksjrd'
+# SECRET_KEY = secrets.SECRET_KEY
+# RUNKEEPER_CLIENT_ID = secrets.RUNKEEPER_CLIENT_ID
+# RUNKEEPER_CLIENT_SECRET = secrets.RUNKEEPER_CLIENT_SECRET
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -141,4 +142,24 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     # other finders..
     'static_precompiler.finders.StaticPrecompilerFinder',
+)
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
