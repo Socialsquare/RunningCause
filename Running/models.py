bf52fdd1052f18a7f0daf799ad8f3bbcb186b4ce
Niles Christensen <nilesnchristensen@gmail.com>
2015-02-17 16:28:36 +0100

    [Removes] what was left of the payment stuff (cleanup).

--- Running/models.py
+++ Running/models.py
@@ -18,10 +18,6 @@ class User(auth.models.AbstractUser):
     is_public = models.BooleanField('Info public?', default=False)
     access_token = models.CharField('Access token', max_length=200, default="", blank=True)
 
-    # def update_sponsorships(self):
-    #     for sponsorship in self.sponsorships_recieved.all():
-    #         sponsorship.update_active_payment()
-
     @property
     def is_runner(self):
         any_runs_or_sponsorships = max(len(self.runs.all()), len(self.sponsorships_recieved.all()))
@@ -66,38 +62,6 @@ class Sponsorship(models.Model):
                 print "SOMETHING HAS GONE VERY WRONG PANIC SPONSORSHIP FIELDS WRONG"
         super(Sponsorship, self).save(*args, **kwargs)
 
-    # @property
-    # def active_payment(self):
-    #     active_payment = self.payments.filter(active=True).first()
-    #     if not self.is_active:
-    #         if active_payment:
-    #             active_payment.active = False
-    #         return None
-    #     if active_payment == None or active_payment.has_ended:
-    #         payment_end_date = min(self.end_date, date.today()+relativedelta(months=1))
-    #         new_payment = Payment(sponsorship=self, end_date=payment_end_date)
-    #         new_payment.save()
-    #         print "New payment created!"
-    #         print ""
-    #         return new_payment
-    #     return active_payment
-
-    
-
-
-
-    # def pay(self):
-    #     for payment in Payment.objects.filter(sponsorship__pk=self.id):
-    #         payment.active = False
-    #         payment.save()
-    #     if (not self.is_active):
-    #         new_payment = Payment.create(sponsorship=self)
-
-    # def update_active_payment(self):
-    #     current_payment = self.active_payment
-    #     if current_payment != None:
-    #         current_payment.update_amount()
-
     def __unicode__(self):
         return '%s' % self.sponsor
 
@@ -111,49 +75,3 @@ class Run(models.Model):
 
     def __unicode__(self):
         return'%s' % self.distance
-
-    # def save(self, *args, **kwargs):
-    #     self.runner.update_sponsorships()
-    #     super(Run, self).save(*args, **kwargs)
-
-
-# class Payment(models.Model):
-#     sponsorship = models.ForeignKey(Sponsorship, related_name='payments')
-#     amount = models.FloatField('Amount', default=0)
-#     active = models.BooleanField('Active', default=True)
-#     start_date = models.DateField('Start Date', default = date.today(), editable=False)
-#     end_date = models.DateField('End date', default=date.today()+relativedelta(months=1))
-
-#     @property
-#     def has_ended(self):
-#         result = (date.today() >= self.end_date)
-#         active = not result
-#         return result
-
-#     def __unicode__(self):
-#         return '%s' % self.sponsorship.sponsor
-
-#     def calculate_amount(self):
-#         rate = self.sponsorship.rate
-#         print "Payment start date: %s" % self.start_date
-#         print "Payment end date: %s" % self.end_date
-#         relevant_runs = self.sponsorship.runner.runs.filter(date__gte=self.start_date,  date__lt=self.end_date)
-#         print "Num relevant runs: %s" % len(relevant_runs)
-#         amount = 0
-#         for run in relevant_runs:
-#             amount = amount + (run.distance * rate)
-#             print "Amount: %s" % amount
-#         print "Final Amount: %s" % amount
-#         return amount
-
-#     def update_amount(self):
-#         print "Updating amount..."
-#         other_payments = self.sponsorship.payments.exclude(pk=self.id)
-#         previous_amount = 0
-#         for payment in other_payments:
-#             previous_amount = previous_amount + payment.amount
-#         print "Previous amount: %s" % previous_amount
-#         current_amount = self.calculate_amount()
-#         print current_amount
-#         self.amount = min(self.sponsorship.max_amount - previous_amount, current_amount)
-#         self.save()
