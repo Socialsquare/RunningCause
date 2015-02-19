8e00190c72c20063ca59dd00b760d1a096934078
Niles Christensen <nilesnchristensen@gmail.com>
2015-02-19 14:34:59 -0500

    [Fixes] invalid syntax in views.py.

--- Running/views.py
+++ Running/views.py
@@ -258,7 +258,7 @@ def register_runkeeper(request, runner_id):
             # Now that we've associated the token with the user, we're done with authentication.
             # Call this view again to finally deal with the workout data.
             reverse('Running.views.register_runkeeper', 
-                        kwargs={'runner_id': runner_id}))
+                        kwargs={'runner_id': runner_id})
 
         # This means that the user has registered with runkeeper before, and has an access token.
         # Use this to get the workouts for the user, and create and store new run objects for
