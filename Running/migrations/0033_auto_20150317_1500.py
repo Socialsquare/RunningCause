3c3307b1f0c408360320108fecd85e480fb3658f
Niles Christensen <nilesnchristensen@gmail.com>
2015-03-17 15:00:11 +0100

    [Adds] a migration.

---
+++ Running/migrations/0033_auto_20150317_1500.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('Running', '0032_auto_20150316_1744'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='run',
+            name='end_date',
+            field=models.DateField(default=datetime.date(2015, 3, 17), verbose_name=b'End Date'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='run',
+            name='start_date',
+            field=models.DateField(default=datetime.date(2015, 3, 17), verbose_name=b'Date'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='sponsorship',
+            name='end_date',
+            field=models.DateField(default=datetime.date(2015, 4, 17), null=True, verbose_name=b'End Date'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='sponsorship',
+            name='start_date',
+            field=models.DateField(default=datetime.date(2015, 3, 17), null=True, verbose_name=b'Start Date'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='wager',
+            name='remind_date',
+            field=models.DateField(default=datetime.date(2015, 4, 17), null=True, verbose_name=b'Remind Date'),
+            preserve_default=True,
+        ),
+    ]
