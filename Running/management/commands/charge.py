ff49fa4c527476bc56ebe32ba2e9fbb854dc97e3
Niles Christensen <nilesnchristensen@gmail.com>
2015-03-19 10:59:05 +0100

    [Changes] charge to only work on the first of the month.

--- Running/management/commands/charge.py
+++ Running/management/commands/charge.py
@@ -14,30 +14,32 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        users_with_customers = User.objects.exclude(stripe_customer_id=None)
-        amount_to_pay = 0
-        for user in users_with_customers:
-            for sponsorship in user.sponsorships_given.all():
-                if sponsorship.left_to_pay > 0:
-                    amount_to_pay = amount_to_pay + sponsorship.left_to_pay
+        if datetime.datetime.today().day == 1:
+
+            users_with_customers = User.objects.exclude(stripe_customer_id=None)
+            amount_to_pay = 0
+            for user in users_with_customers:
+                for sponsorship in user.sponsorships_given.all():
+                    if sponsorship.left_to_pay > 0:
+                        amount_to_pay = amount_to_pay + sponsorship.left_to_pay
+                        print amount_to_pay
+                for wager in user.wagers_given.filter(paid=False, fulfilled=True):
+                    amount_to_pay = amount_to_pay + wager.amount
                     print amount_to_pay
-            for wager in user.wagers_given.filter(paid=False, fulfilled=True):
-                amount_to_pay = amount_to_pay + wager.amount
                 print amount_to_pay
-            print amount_to_pay
-            if amount_to_pay!=0:
-                stripe_status = stripe.Charge.create(
-                    amount=int(amount_to_pay*100),
-                    currency="dkk",
-                    customer=user.stripe_customer_id
-                )["status"] 
-                if stripe_status == "succeeded":
-                    for sponsorship in user.sponsorships_given.all():
-                        sponsorship.amount_paid = sponsorship.total_amount
-                        sponsorship.save()
-                    for wager in user.wagers_given.filter(paid=False, fulfilled=True):
-                        wager.paid = True
-                        wager.save()
+                if amount_to_pay!=0:
+                    stripe_status = stripe.Charge.create(
+                        amount=int(amount_to_pay*100),
+                        currency="dkk",
+                        customer=user.stripe_customer_id
+                    )["status"] 
+                    if stripe_status == "succeeded":
+                        for sponsorship in user.sponsorships_given.all():
+                            sponsorship.amount_paid = sponsorship.total_amount
+                            sponsorship.save()
+                        for wager in user.wagers_given.filter(paid=False, fulfilled=True):
+                            wager.paid = True
+                            wager.save()
 
 
-                print "{0} charged {1}".format(user.username, amount_to_pay)
+                    print "{0} charged {1}".format(user.username, amount_to_pay)
