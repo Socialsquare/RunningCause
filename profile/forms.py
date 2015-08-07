de6e4ab50587b050ca1959d90e1712c7f94fae71
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-08-07 18:34:12 +0200

    Rearranged the sign-up headings and made the newsletter and publically visible profile initially true.

--- profile/forms.py
+++ profile/forms.py
@@ -4,16 +4,17 @@ from django import forms
 
 
 class SignupForm(forms.Form):
-    public_info = forms.BooleanField(label="Do you want your sponsorships to be publicly visible?",
+    public_info = forms.BooleanField(label="I want my sponsorships to be publicly visible",
                                      widget=forms.CheckboxInput(),
-                                     required=False)
+                                     required=False,
+                                     initial=True)
 
-    newsletter = forms.BooleanField(label="Would you like to recieve the Masanga newsletter?",
+    newsletter = forms.BooleanField(label="I would like to recieve the Masanga newsletter",
                                     widget=forms.CheckboxInput(),
-                                    required=False)
+                                    required=False,
+                                    initial=True)
 
     def signup(self, request, user):
         user.is_public = self.cleaned_data['public_info']
         user.newsletter = self.cleaned_data['newsletter']
         user.save()
-
