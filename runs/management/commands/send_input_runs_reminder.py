5e21c8ed2e868eee9bcdf8c57f9231b721988496
pawciobiel <pawciobiel@gmail.com>
2015-08-20 17:12:06 +0200

    fix notify about runs reminder email and command

--- runs/management/commands/send_input_runs_reminder.py
+++ runs/management/commands/send_input_runs_reminder.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+import calendar
+
 from django.core.management.base import BaseCommand
 
 from runs.tasks import send_input_runs_reminder
@@ -9,5 +12,10 @@ class Command(BaseCommand):
     help = 'Send input your runs reminder'
 
     def handle(self, *args, **options):
-        send_input_runs_reminder()
-        print 'Reminder emails have been sent'
+        todays = datetime.date.today()
+        last_day = calendar.monthrange(todays.year, todays.month)[1]
+        if last_day == todays.day:
+            send_input_runs_reminder()
+            print 'Reminder emails have been sent'
+        else:
+            print 'This command runs only on the last day of the month'
