49db54cc3555398ebdedf52c46bbc247724e6830
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-02-14 12:42:28 +0100

    Updated the way challenge reminders are sent.

--- challenges/tasks.py
+++ challenges/tasks.py
@@ -12,42 +12,43 @@ from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.template import loader, Context
 
+from common.helpers import send_email
+
+from .models import Challenge
+
 
 log = get_task_logger(__name__)
 
 
-def send_challenge_reminder(user_id=None):
-    from .models import Challenge
+def send_challenge_reminder(user_id):
     user = get_user_model().objects.get(id=user_id)
-    todays = datetime.date.today()
-    remind_delta = todays + Challenge.get_remind_date_delta()
-    received_ending_soon_challenges = user.challenges_recieved\
-        .filter(status=Challenge.ACTIVE, end_date=remind_delta)
+    today = datetime.date.today()
+    filters = {
+        'status': Challenge.ACTIVE,
+        'end_date': today
+    }
+    ending_challenges = user.challenges_recieved.filter(**filters)
 
-    decision_delta = todays - Challenge.get_decision_date_delta()
-    given_completed_challenges = user.challenges_given\
-        .filter(status=Challenge.COMPLETED, end_date=decision_delta)
-
-    if not received_ending_soon_challenges and not given_completed_challenges:
-        log.debug("user %s has no completed or ending soon challenges", user.id)
-        return
-
-    subject = _('Masanga Runners challenge reminder')
-    tmpl_name = 'challenges/emails/challenges_reminder.html'
-    tmpl = loader.get_template(tmpl_name)
-    ctx = Context({
-        'received_ending_soon_challenges': received_ending_soon_challenges,
-        'given_completed_challenges': given_completed_challenges,
+    email_subject = _('Challenge ends today!')
+    email_context = {
+        'ending_challenges': ending_challenges,
         'BASE_URL': settings.BASE_URL,
-    })
-    html_msg = tmpl.render(ctx)
-    send_mail(subject, '', settings.DEFAULT_FROM_EMAIL, [user.email],
-              fail_silently=False, html_message=html_msg)
+    }
+
+    send_email([user.email],
+               email_subject,
+               'challenges/emails/challenges_reminder.html',
+               email_context)
 
 
 @shared_task(ignore_result=True)
 def send_challenge_reminders():
-    active_users_ids = get_user_model().objects.filter(is_active=True)\
-        .values_list('id', flat=True)
-    for user_id in active_users_ids:
-        send_challenge_reminder(user_id=user_id)
+    # Fetch runners that has challenges ending today.
+    today = datetime.date.today()
+    filters = {
+        'is_active': True,
+        'challenges_recieved__end_date': today
+    }
+    relevant_runners = get_user_model().objects.filter(**filters)
+    for runner in relevant_runners:
+        send_challenge_reminder(runner.id)
--- common/helpers.py
+++ common/helpers.py
@@ -1,10 +1,14 @@
+from django.conf import settings
 from django.core.mail import EmailMessage
+from django.template import loader, Context
+from django.utils import translation
 
 
 def send_email(to_list, subject, message_template, message_context):
+    translation.activate(settings.LANGUAGE_CODE)
     context = Context(message_context)
     message = loader.get_template(message_template).render(context)
     sender = settings.DEFAULT_FROM_EMAIL
     msg = EmailMessage(subject, message, sender, to_list)
     msg.content_subtype = "html"
-    return msg.send()
+    return msg.send(fail_silently=True)
