9f455f23bb0e2fdc6d9c3b3efb9d74a7d2f0971a
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-03-07 23:06:36 +0100

    Adding a forced option and changed to the 26th of the month

--- runs/management/commands/send_input_runs_reminder.py
+++ runs/management/commands/send_input_runs_reminder.py
@@ -11,11 +11,19 @@ from runs.tasks import send_input_runs_reminder
 class Command(BaseCommand):
     help = 'Send input your runs reminder'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--forced',
+                            action='store_true',
+                            dest='forced',
+                            default=False,
+                            help='Force the sending of reminders')
+
     def handle(self, *args, **options):
         todays = datetime.date.today()
-        last_day = calendar.monthrange(todays.year, todays.month)[1]
-        if last_day == todays.day:
-            send_input_runs_reminder()
-            print 'Reminder emails have been sent'
+        #last_day = calendar.monthrange(todays.year, todays.month)[1]
+        if todays.day == 26 or options.get('forced'):
+            emails_sent = send_input_runs_reminder()
+            print 'A total of %d reminder emails have been sent' % emails_sent
         else:
-            print 'This command runs only on the last day of the month'
+            print('This command runs only on the 26th day of the month '
+                  'use the --forced to do it anyway.')
