f94110a91db9f0e52209e470b6ed8c4b4b3fe30c
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-07-27 10:37:15 +0200

    Allow a single email as the to email field

--- common/helpers.py
+++ common/helpers.py
@@ -7,6 +7,9 @@ def send_email(to_list, subject, message_template, message_context):
     message_context.update({
         'BASE_URL': settings.BASE_URL
     })
+    # Turn a single email into a list of one element
+    if isinstance(to_list, str) or isinstance(to_list, unicode):
+        to_list = [to_list, ]
     context = Context(message_context)
     message = loader.get_template(message_template).render(context)
     sender = settings.DEFAULT_FROM_EMAIL
