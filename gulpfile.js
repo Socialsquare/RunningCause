2b08c26b7dac86002fd10814da1668530629c30f
Andreas Larsen <andreas@larsenwork.com>
2016-07-27 12:13:55 +0200

    style cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,48 @@
-// var gulp = require('gulp');
-// var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
 // var concat = require('gulp-concat');
 // var del = require('del');
 // var livereload = require('gulp-livereload');
 // var minify = require('gulp-minify');
-// var minifycss = require('gulp-minify-css');
-// var plumber = require('gulp-plumber');
-// var rename = require('gulp-rename');
-// var runSequence = require('run-sequence');
-// var sass = require('gulp-sass');
+var cleancss = require('gulp-clean-css');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
 // var svgmin = require('gulp-svgmin');
 // var svgstore = require('gulp-svgstore');
 // var watch = require('gulp-watch');
 //
 //
-// // 2. FILE PATHS
-// // - - - - - - - - - - - - - - -
-//
-// var paths = {
-//   static: [
-//     'assets/static/'
-//   ],
-//   sass: [
-//     'src/css/base.scss'
-//   ],
-//   sassWatch: [
-//     'src/css/**/*.scss',
-//   ],
-//   js: [
-//     'node_modules/svg4everybody/dist/svg4everybody.js',
-//     'src/js/*.js'
-//   ],
-//   svg: [
-//     'src/img/*.svg'
-//   ]
-// }
+// 2. FILE PATHS
+// - - - - - - - - - - - - - - -
+
+var staticDir = 'RunningCause/static/'
+
+var paths = {
+  css: [
+    staticDir + 'css/'
+  ]
+}
+
+var files = {
+  static: [
+    staticDir
+  ],
+  sass: [
+    paths.css + '*.scss'
+  ],
+  sassWatch: [
+    paths.css + '**/*.scss'
+  ],
+  js: [
+    'node_modules/svg4everybody/dist/svg4everybody.js',
+    'src/js/*.js'
+  ],
+  svg: [
+    'src/img/*.svg'
+  ]
+}
 //
 // // 3. TASKS
 // // - - - - - - - - - - - - - - -
@@ -44,26 +52,25 @@
 //   return del(paths.static + '*');
 // })
 //
-// // Compiles Sass
-// gulp.task('sass', function() {
-//   return gulp.src(paths.sass)
-//     .pipe(plumber({
-//       errorHandler: function(err) {
-//         console.log(err);
-//         this.emit('end');
-//       }
-//     }))
-//     .pipe(sass())
-//     .pipe(autoprefixer({
-//       browsers: ['last 4 versions', 'ie 10']
-//     }))
-//     .pipe(gulp.dest('.tmp/css'))
-//     .pipe(rename({
-//       suffix: '.min'
-//     }))
-//     .pipe(minifycss())
-//     .pipe(gulp.dest(paths.static + ''));
-// });
+// Compiles Sass
+gulp.task('sass', function() {
+  return gulp.src(files.sass)
+    .pipe(plumber({
+      errorHandler: function(err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions', 'ie 10']
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(cleancss())
+    .pipe(gulp.dest(paths.css + ''));
+});
 //
 // // Compiles JS
 // gulp.task('js', function() {
@@ -100,17 +107,19 @@
 //     });
 //
 //
-// gulp.task('watch', function() {
-//   // Watch Sass
-//   gulp.watch(paths.sassWatch, ['sass']);
-//
-//   // Watch javascript
-//   gulp.watch(paths.js, ['js']);
-//
-//   // Watch svg
-//   gulp.watch(paths.svg, ['svg-sprite']);
-//
-// });
+gulp.task('watch', function() {
+  // Watch Sass
+  gulp.watch(files.sassWatch, ['sass']);
+
+  // Watch javascript
+  // gulp.watch(paths.js, ['js']);
+
+  // Watch svg
+  // gulp.watch(paths.svg, ['svg-sprite']);
+
+});
+
+
 //
 // // Builds your entire app once, without starting a server
 // gulp.task('build', function(callback) {
@@ -126,3 +135,13 @@
 //     'watch',
 //     callback);
 // });
+
+
+// Default task: builds your app, starts a server, and recompiles assets when they change
+gulp.task('default', function(callback) {
+  runSequence(
+    // 'clean',
+    ['sass'],
+    'watch',
+    callback);
+});
