6d7060802373698a6fcdc96d7d2636a264740034
Andreas Larsen <andreas@larsenwork.com>
2016-07-28 16:44:20 +0200

    handle merge conflicts

--- invitations/forms.py
+++ invitations/forms.py
@@ -1,6 +1,6 @@
 # coding: utf8
 from django import forms
-
+from django.utils.translation import ugettext as _
 
 class EmailInviteForm(forms.Form):
-    email = forms.EmailField(label="Friend's email address")
+    email = forms.EmailField(label=_("Friend's email address"))
